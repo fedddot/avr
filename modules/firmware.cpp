cfb77f6213ffaf362c6e8989918ce29543d6450d
idan.rom <idanrommm@gmail.com>
2022-09-24 19:34:52 +0300

    firmware.cpp - conf. testing for UART. TODO - test transciever functionality (after implementation

--- modules/firmware.cpp
+++ modules/firmware.cpp
@@ -1,35 +1,100 @@
-#define __AVR_ATmega328P__ 1
-#define F_CPU 8000000UL
-
 #include <avr/io.h>
+#include <alloca.h>
 #include <util/delay.h>
+#include <string.h>
 
-#include "register.h"
-#include "gpio.h"
+#include "uart.h"
 
-#define WPIN BIT5
+#define WPINNUM 5
+#define WPORT PORTB
+#define WDDR DDRB
+#define WPIN PINB
+
+#define ASCII_0 0x30
+#define ASCII_9 0x39
+#define ASCII_A 0x41
+#define ASCII_Z 0x5A
+
+#define BASE_10 10
+
+#define ASCII_LEN 128
+
+
+void USART_Transmit(uint8_t *data, uint8_t num)
+{
+
+    while (num) {
+        while (!(UCSR0A & (1<<UDRE0))) {
+            ;
+        }
+        UDR0 = *data;
+        ++data;
+        --num;
+    }
+}
+
+char *ItoaAnyBaseRecursive(int value, char *str, unsigned int radix)
+{
+    if ((value < (int)radix) && (value > -(int)radix))
+    {
+        if (value < 0)
+        {
+            *str = '-';
+            ++str;
+        }      
+    }
+    else
+    {
+        str = ItoaAnyBaseRecursive(value / (int)radix, str, radix);
+        value = value % (int)radix;
+    } 
+    if (value < 0)
+    {
+        value = -value;
+    }
+    if (value < BASE_10)
+    {
+        *str = (char)value + ASCII_0;
+    }
+    else
+    {
+        *str = (char)value - BASE_10 + ASCII_A;
+    }
+    return str + 1;  
+}
+
+char *ItoaBaseTen(int value, char *str)
+{
+    char *str_end = NULL;
+
+    str_end = ItoaAnyBaseRecursive(value, str, 10);
+    *str_end = '\0';
+    return str;
+}
 
 
 int main() {
-    GPIO::GPIO gpio = GPIO::GPIO(
-        Register::Register((uint8_t *)&PORTB),
-        Register::Register((uint8_t *)&DDRB),
-        Register::Register((uint8_t *)&PINB)
-    );    
-    gpio.set_mcucr_reg(
-        Register::Register((uint8_t *)&MCUCR)
-    );
-    gpio.set_pud_bit(Register::BIT4);
-    gpio.set_pin_cfg(Register::BIT5, GPIO::PIN_CFG_OUTPUT);
-    gpio.set_pin(Register::BIT5, Register::BIT_LOW);
+    char *data = (char *)alloca(20);
+    uint8_t datalen = 20;
 
+    UART uart = UART();
+
+    WDDR |= 1 << WPINNUM;
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    
     while (1)
     {
-        gpio.set_pin(Register::BIT5, Register::BIT_LOW);
-        _delay_ms(1000);
-        gpio.set_pin(Register::BIT5, Register::BIT_HIGH);
+        WPORT |= 1 << WPINNUM;
         _delay_ms(1000);
 
+        data = strcpy(data, "baud: ");
+        USART_Transmit((uint8_t *)data, strlen(data));
+        data = ItoaBaseTen((int)uart.get_baud_rate(), data);
+        USART_Transmit((uint8_t *)data, strlen(data));
+        data = strcpy(data, "\n");
+        USART_Transmit((uint8_t *)data, strlen(data));
+        WPORT &= ~(1 << WPINNUM);
+        _delay_ms(1000);
     }
 
     return 0;
